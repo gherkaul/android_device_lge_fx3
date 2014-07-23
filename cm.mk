7ea36277b1f4193dfe34faedf79be72d814a387b
Joshua <xclusive36@users.noreply.github.com>
2014-07-23 09:28:23 -0400

    Create cm.mk

---
+++ cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := fx3
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/fx3s/device_fx3s.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := fx3s
+PRODUCT_NAME := cm_fx3s
+PRODUCT_BRAND := VirginMobile
+PRODUCT_MODEL := VM720
+PRODUCT_MANUFACTURER := LGE

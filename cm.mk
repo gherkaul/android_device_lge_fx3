6540c31394747399134937f645b331613ccfa44f
Joshua <xclusive36@users.noreply.github.com>
2014-07-24 16:10:44 -0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -10,11 +10,11 @@ TARGET_BOOTANIMATION_NAME := vertical-320x480
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/fx3s/device_fx3s.mk)
+$(call inherit-product, device/lge/fx3/device_fx3.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := fx3s
-PRODUCT_NAME := cm_fx3s
+PRODUCT_DEVICE := fx3
+PRODUCT_NAME := cm_fx3
 PRODUCT_BRAND := VirginMobile
 PRODUCT_MODEL := VM720
 PRODUCT_MANUFACTURER := LGE

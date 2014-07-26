f6f05c7c21ace4b61571cb617e4707e50dfa96c1
Joshua <xclusive36@users.noreply.github.com>
2014-07-26 12:51:52 -0400

    adding stuff

--- device_fx3.mk
+++ device_fx3.mk
@@ -70,6 +70,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnetcmdiface
 
+# Wifi config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:/system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:/system/etc/wifi/wpa_supplicant_overlay.conf
+    
 # Audio config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \

252134c36be1425498ac38c95273fb1cd36d6635
Joshua <xclusive36@users.noreply.github.com>
2014-08-02 13:55:52 -0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -20,3 +20,16 @@ PRODUCT_NAME := cm_fx3
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-LS720
 PRODUCT_MANUFACTURER := LGE
+
+    CDMA_GOOGLE_BASE := android-sprint-us
+    CDMA_CARRIER_ALPHA := Virgin_Mobile
+    CDMA_CARRIER_NUMERIC := 311490
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase.ms=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.am=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.yt=$(CDMA_GOOGLE_BASE) \
+    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC)

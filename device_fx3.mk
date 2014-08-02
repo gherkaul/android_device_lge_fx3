d0341e4ea8d1c3af4cce497d8365337fe4b7dc53
Joshua <xclusive36@users.noreply.github.com>
2014-08-02 13:54:54 -0400

    Update device_fx3.mk

--- device_fx3.mk
+++ device_fx3.mk
@@ -24,19 +24,6 @@ $(call inherit-product-if-exists, vendor/lge/fx3/fx3-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/fx3/overlay
 
-CDMA_GOOGLE_BASE := android-sprint-us
-CDMA_CARRIER_ALPHA := Virgin_Mobile
-CDMA_CARRIER_NUMERIC := 311490
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase.ms=$(CDMA_GOOGLE_BASE) \
-    ro.com.google.clientidbase.am=$(CDMA_GOOGLE_BASE) \
-    ro.com.google.clientidbase.yt=$(CDMA_GOOGLE_BASE) \
-    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
-    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
-    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
-    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC)
-
 LOCAL_PATH := device/lge/fx3
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
